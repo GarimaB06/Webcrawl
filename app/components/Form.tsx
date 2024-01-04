e92e49b734fb6e6d71c67aa02e0e1bbc1b18bbd9
GarimaB06 <marigolddancestudio@gmail.com>
2024-01-04 01:00:56 -0500

    basic input UI & button

---
+++ app/components/Form.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import "../globals.css";
+
+const Form = () => {
+	return (
+		<form>
+			<input />
+			<button>Submit</button>
+		</form>
+	);
+};
+
+export default Form;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-
+import Form from "./components/Form";
+import "./globals.css";
 export const metadata: Metadata = {
 	title: "Webcrawl",
 	description: "Visualize your web crawled data",
@@ -14,6 +15,9 @@ export default function RootLayout({
 		<html>
 			<body>
 				<h1>Welcome to Webcrawl</h1>
+				<p>Enter your website URL</p>
+				<p>Required Format: https://www.example.com</p>
+				<Form />
 			</body>
 		</html>
 	);
